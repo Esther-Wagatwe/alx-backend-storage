68a3291c2c2a5042d7b460da1ef0b49707d491e7
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-08-22 19:19:18 +0300

    Add description

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,7 +1,12 @@
--- Drop the view if it already exists
-DROP VIEW IF EXISTS need_meeting;
+-- Write a SQL script that creates a view need_meeting that lists all
+-- students that have a score under 80 (strict) and no last_meeting or
+-- more than 1 month.
 
--- Create the view
+-- Requirements:
+-- The view need_meeting should return all students name when:
+-- They score are under (strict) to 80
+-- AND no last_meeting date OR more than a monthDROP VIEW IF EXISTS need_meeting;
+
 CREATE VIEW need_meeting AS
 SELECT name
 FROM students
