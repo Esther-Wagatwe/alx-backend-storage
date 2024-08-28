cc3ea8c9a597304f2aae77da8f13df0ab825a156
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-08-28 09:46:26 +0300

    Incrementing values

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,6 +3,18 @@
 import redis
 from typing import Union, Optional, Callable
 import uuid
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """Decorator to count how many times a method is called."""
+    @wraps(method)
+    def wrapper(self, *args, **kwargs) -> any:
+        """Wrap the original method, increment the call count
+        return the method's result."""
+        self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 
 class Cache:
@@ -10,9 +22,10 @@ class Cache:
     def __init__(self) -> None:
         """Initialize the Cache instance with a Redis client
         and flush the database."""
-        self.__redis = redis.Redis()
-        self.__redis.flushdb(True)
+        self._redis = redis.Redis()
+        self._redis.flushdb(True)
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Store the given data in Redis using a randomly generated key.
@@ -24,20 +37,23 @@ class Cache:
             str: The randomly generated key used to store the data.
         """
         key = str(uuid.uuid4())
-        self.__redis.set(key, data)
+        self._redis.set(key, data)
         return key
+
     def get(
             self,
             key: str,
             fn: Optional[Callable] = None
             ) -> Union[str, bytes, int, float, None]:
-        data = self.__redis.get(key)
+        data = self._redis.get(key)
         return fn(data) if fn is not None else data
-    
+
     def get_str(self, key: str) -> str:
-        """Retrieve data from Redis and automatically convert it to a string."""
+        """Retrieve data from Redis and automatically convert it to a string.
+        """
         return self.get(key, lambda x: x.decode('utf-8'))
 
     def get_int(self, key: str) -> int:
-        """Retrieve data from Redis and automatically convert it to an integer."""
+        """Retrieve data from Redis and automatically convert it to an integer.
+        """
         return self.get(key, lambda x: int(x))
